fbc98763c0533231e7f7bad358f3789b15e6f4d6
melodyekbatani <melodyekbatani@gmail.com>
2026-02-22 18:33:20 -0500

    Modal from course website

---
+++ script.js
@@ -0,0 +1,23 @@
+// from class https://typography-interaction-2526.github.io/topic/javascript/#opening-a-modal 
+
+
+// Similar to before, setting up variables.
+let modalButton = document.querySelector('#modal') // The thing we’re clicking.
+let modalDialog = document.querySelector('#dialog') // Now one for our `dialog`.
+let closeButton = modalDialog.querySelector('button') // Only looking within `modalDialog`.
+
+modalButton.addEventListener('click', () => { // “Listen” for clicks.
+	modalDialog.showModal() // This opens it up.
+})
+
+closeButton.addEventListener('click', () => {
+	modalDialog.close() // And this closes it!
+})
+
+// Listen to *all* clicks, now including the `event` parameter…
+document.addEventListener('click', (event) => {
+	// Only clicks on the page itself behind the `dialog`.
+	if (event.target == document.documentElement) {
+		modalDialog.close() // Close it too then.
+	}
+})
